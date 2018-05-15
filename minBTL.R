2a1efd0ad5c7f992a160bf1b60522b3802f2a7c4
RolyYang <38229007+RolyYang@users.noreply.github.com>
2018-05-15 14:27:26 -0400

    minimum backtest length
    academic reference:Pseudo-Mathematics and Financial Charlatanism: The Effects of Backtest Overfitting on Out-of-Sample Performance(David H. Bailey)

---
+++ minBTL.R
@@ -0,0 +1,25 @@
+minBTL<- function(Emax=1.0,N) {
+	
+	gama<-0.5772156649		#Euler-Mascheroni constant
+	minBTL<-(((1-gama)*qnorm(1-1/N)+gama*qnorm(1-1/N*exp(-1)))/Emax)**2
+  minBTL
+	
+}
+
+minBTLcon<- function(Emax=1.0,N) {
+	minBTLcon<-2*log(N)/Emax
+  minBTLcon
+}
+exp<-c()
+max<-c()
+for (i in 1:1000){
+	exp[i]<-minBTL(1.0,i)
+	max[i]<-minBTLcon(1.0,i)
+	next
+}
+length<-c(1:1000)
+plot(length,exp,main="Minimum Back Test Length",type="l",xlab="Number of Trials (N)",ylab="Mimunum Backtest Length (in Year)")
+line(length,max)
+
+N<-9
+minBTLcon(1.0,9) #4.39445
